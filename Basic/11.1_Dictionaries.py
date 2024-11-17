15ca8288b2041006d6d92dd75cdfc4b3f9b020e8
rafi076 <mohiuddinrafi076@gmail.com>
2024-11-18 00:29:49 +0600

    prac

--- Basic/11.1_Dictionaries.py
+++ Basic/11.1_Dictionaries.py
@@ -1,45 +1,75 @@
-# Dic tionaries are ordered collection of data items.
-# They dtore multiple items in a singel variable.
-# Dictionary items are key value pairs that are separated by commas
-# and enclosed within curly brackets.dictionaries store item inorder way!!
+# # Dic tionaries are ordered collection of data items.
+# # They dtore multiple items in a singel variable.
+# # Dictionary items are key value pairs that are separated by commas
+# # and enclosed within curly brackets.dictionaries store item inorder way!!
 
-info = {
-    'name':'rafi',
-    'age': 24,
-    'elibible':True,
-    76:"mohin"
-}
-print(info["name"]) # rafi
-print(info[76]) # mohin
-print(info) # {'name': 'rafi', 'age': 24, 'elibible': True, 76: 'mohin'}
-print(info.keys()) # (['name', 'age', 'elibible', 76])
-# Or
-for key in info.keys():
-    print(info[key]) # rafi 24 True mohin
+# info = {
+#     'name':'rafi',
+#     'age': 24,
+#     'elibible':True,
+#     76:"mohin"
+# }
+# print(info["name"]) # rafi
+# print(info[76]) # mohin
+# print(info) # {'name': 'rafi', 'age': 24, 'elibible': True, 76: 'mohin'}
+# print(info.keys()) # (['name', 'age', 'elibible', 76])
+
+# # Or
+# for key in info.keys():
+#     print(info[key]) # rafi 24 True mohin
     
-print(info.values()) # ['rafi', 24, True, 'mohin']
-# Or
-for key in info.keys():
-    print(f"The values corresponding to the {key} is {info[key]}")
+# print(info.values()) # ['rafi', 24, True, 'mohin']
 
-#The values corresponding to the name is rafi
-# The values corresponding to the age is 24
-# The values corresponding to the elibible is True
-# The values corresponding to the 76 is mohin
 
-############### Dictionaries Method ######################
-seniors = {222:98, 212:78, 312:95,132:77, 342:69}
-juniors = {111:75, 333:47, 442:66, 553:78}
-# seniors.update(juniors)
-# print(seniors) # after updating: {222: 98, 212: 78, 312: 95, 132: 77, 342: 69, 111: 75, 333: 47, 442: 66, 553: 78}
+# # Or
+# for key in info.keys():
+#     print(f"The values corresponding to the {key} is {info[key]}")
 
-seniors.pop(222) # to pop specific item
-print(seniors) # {212: 78, 312: 95, 132: 77, 342: 69}
+# #The values corresponding to the name is rafi
+# # The values corresponding to the age is 24
+# # The values corresponding to the elibible is True
+# # The values corresponding to the 76 is mohin
 
-# to pop last item
-seniors.popitem()
-print(seniors) # {212: 78, 312: 95, 132: 77}
 
-# to delete an item
-del seniors[212]
-print(seniors) # {312: 95, 132: 77}
+
+
+
+
+# ############### Dictionaries Method ######################
+# seniors = {222:98, 212:78, 312:95,132:77, 342:69}
+# juniors = {111:75, 333:47, 442:66, 553:78}
+# # seniors.update(juniors)
+# # print(seniors) # after updating: {222: 98, 212: 78, 312: 95, 132: 77, 342: 69, 111: 75, 333: 47, 442: 66, 553: 78}
+
+# seniors.pop(222) # to pop specific item
+# print(seniors) # {212: 78, 312: 95, 132: 77, 342: 69}
+
+# # to pop last item
+# seniors.popitem()
+# print(seniors) # {212: 78, 312: 95, 132: 77}
+
+# # to delete an item
+# del seniors[212]
+# print(seniors) # {312: 95, 132: 77}
+
+
+
+
+# Create an empty dictionary
+user_dict = {}
+
+# Ask the user how many key-value pairs they want to add
+n = int(input("How many items do you want to add to the dictionary? "))
+
+# Loop to get user inputs
+for i in range(n):
+    key = input(f"Enter key {i+1}: ")
+    value = input(f"Enter value for key '{key}': ")
+    user_dict[key] = value  # Add the key-value pair to the dictionary
+
+# Print the final dictionary
+print("\nThe final dictionary is:")
+print(user_dict)
+
+
+
